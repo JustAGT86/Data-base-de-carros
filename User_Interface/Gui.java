0b7f1101ba127353ce26597aecdacc2768c70561
João Pedro Arcuri Baetas <85511157+JustAGT86@users.noreply.github.com>
2023-10-04 01:50:58 -0300

    Add files via upload

---
+++ User_Interface/Gui.java
@@ -0,0 +1,10 @@
+package User_Interface;
+import javax.swing.JOptionPane;
+
+public class Gui {
+    String classe= JOptionPane.showInputDialog("Digite a classe");
+        String marca= JOptionPane.showInputDialog("Digite a marca");
+        String ano= JOptionPane.showInputDialog("Digite o ano");
+        String nome= JOptionPane.showInputDialog("Digite o nome");
+        String código= JOptionPane.showInputDialog("Digite o codigo");
+}
